c8b6feb06a88bd10efc69ba4127b508fd7894a3c
Elias <elias.gilbert@ymail.com>
2016-08-31 17:39:18 -0400

    Added Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include "Student.h"
+#include <string>
+using namespace std;
+
+Student::Student()
+{
+	name="";
+	usf_id="";
+	email="";
+	double grade_presentation=0.0;
+	double grade_essay1=0.0;
+	double grade_essay2=0.0;
+	double grade_termProject=0.0;
+}
