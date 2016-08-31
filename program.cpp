32b6d19ce442bd61f89b4f7cb79e7f57726248e1
epretorius01 <pretorius.emily@yahoo.com>
2016-08-31 17:16:17 -0400

    toStringMethodAddDoubleQuotes
    Allowed for " to be inside strings

--- program.cpp
+++ program.cpp
@@ -10,7 +10,40 @@ int main(int argc, char* argv[])
 
 
 string toString(Student *student) {
-        string csvString = "\"" + student->name + "\", \"" + student->usf_id + "\", \"" + student->email + "\", \"" + to_string(student->grade_presentation) + "\", \"" + to_string(student->grade_essay1) + "\", \"" + to_string(student->grade_essay2) + "\", \"" + to_string(student->grade_termProject) + "\"";
+
+        string replaceComma = "\"\"";
+
+        // Check Student name for commas
+        for (int i = 0; i < student->name.length(); i++)
+        {
+                if (student->name[i] == '"')
+                {
+                        student->name.replace(i, 1, replaceComma);
+                        i += 2;
+                }
+        }
+
+        // Check Student ID for commas
+        for (int i = 0; i < student->usfID.length(); i++)
+        {
+                if (student->usfID[i] == '"')
+                {
+                        student->usfID.replace(i, 1, replaceComma);
+                        i += 2;
+                }
+        }
+
+        // Check Student email for commas
+        for (int i = 0; i < student->email.length(); i++)
+        {
+                if (student->email[i] == '"')
+                {
+                        student->email.replace(i, 1, replaceComma);
+                        i += 2;
+                }
+        }
+
+        string csvString = "\"" + student->name + "\", \"" + student->usfID + "\", \"" + student->email + "\", \"" + to_string(student->gradeOfPresentation) + "\", \"" + to_string(student->gradeOfEssay1) + "\", \"" + to_string(student->gradeOfEssay2) + "\", \"" + to_string(student->gradeofTermProject) + "\"";
 
         //string stringTesting = "Student Name: " + student->name + "," + "USF ID: " + student->usfID+ "," + "Email: " + student->email + "," + "Grade of presentation: " + student->gradeOfPresentation + "," + "Grade of Essay No. 1: " + student->gradeOfEssay1 + "," + "Grade of Essay No. 2: " + student->gradeOfEssay2 + "," + "Grade of the term project: " + student->gradeofTermProject;
         return(csvString);
