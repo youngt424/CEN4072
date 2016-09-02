10c9589b76bad8b4370577aec27d9c7f964cd9e7
EliasEGilbert <elias.gilbert@ymail.com>
2016-09-01 17:08:21 -0700

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -11,13 +11,13 @@ Student::Student()
 	grade_termProject=0;
 }
 
-Student::Student(string nm, string id, string emal, double g_presentaion, double g_essay1, double g_essay2, double g_termProject)
+Student::Student(string nm, string id, string emal, double g_presentation, double g_essay1, double g_essay2, double g_termProject)
 {
 	name = nm;
 	usf_id  = id;
 	email = emal;
 	grade_presentation = g_presentation;
-    grade_essay1 = g_essay1;
+    	grade_essay1 = g_essay1;
 	grade_essay2 = g_essay2;
 	grade_termProject = g_termProject;
 }
