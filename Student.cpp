2eeb0b51d3396b264ff2e6695ebba5a603cd071d
EliasEGilbert <elias.gilbert@ymail.com>
2016-09-14 15:06:18 -0700

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -54,6 +54,43 @@ Student::Student(vector<string> stu_info)
 	}
 }
 
+string Student::toString(Student *student) {
+
+        string replace = "\"\"";
+
+        for (int i = 0; i < student->name.length(); i++)
+        {
+                if (student->name[i] == '"')
+                {
+                        student->name.replace(i, 1, replace);
+                        i += 2;
+                }
+        }
+
+        for (int i = 0; i < student->usf_id.length(); i++)
+        {
+                if (student->usf_id[i] == '"')
+                {
+                        student->usf_id.replace(i, 1, replace);
+                        i += 2;
+                }
+        }
+
+        for (int i = 0; i < student->email.length(); i++)
+        {
+                if (student->email[i] == '"')
+                {
+                        student->email.replace(i, 1, replace);
+                        i += 2;
+                }
+        }
+
+        string csvString = "\"" + student->name + "\", \"" + student->usf_id + "\", \"" + student->email + "\", \"" + to_string(student->grade_presentation) + "\", \"" + to_string(student->grade_essay1) + "\", \"" + to_string(student->grade_essay2) + "\", \"" + to_string(student->grade_termProject) + "\"";
+
+        //string stringTesting = "Student Name: " + student->name + "," + "USF ID: " + student->usfID+ "," + "Email: " + student->email + "," + "Grade of presentation: " + student->gradeOfPresentation + "," + "Grade of Essay No. 1: " + student->gradeOfEssay1 + "," + "Grade of Essay No. 2: " + student->gradeOfEssay2 + "," + "Grade of the term project: " + student->gradeofTermProject;
+        return(csvString);
+}
+
 Student::~Student()
 {
 }
