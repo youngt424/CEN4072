79b394cf393daaf33dc1bdab7fb4922b22c7686f
Elias <elias.gilbert@ymail.com>
2016-09-28 19:17:38 -0400

    Added comments to studentDao class

--- studentDao.h
+++ studentDao.h
@@ -8,7 +8,6 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
-#include <ctime>
 #include "student.h"
 
 using namespace std;
@@ -22,20 +21,57 @@ class StudentDao
 	
 	public:
 		StudentDao();
+		//Creates dao from student data found in file "location"
 		StudentDao(string location);
-
+		
+		/*
+		Precondition: None
+		Postcondition: returns true if there is a file location set for saving
+		*/
 		bool canSave();
+		
+		/*
+		Precondition: None
+		Postcondition: student data is saved to a specified file
+		*/
 		void save();
+		
+		/*
+		Precondition: an input string as a file location
+		Postcondition: student data is saved to the specified file
+		*/
 		void saveAs(string location);
 		
 		int count();
 		
+		/*
+		Precondition: a student record 
+		Postcondition: the student information is saved to the dao
+		*/
 		void addRecord(Student* record);
-		Student* readRecord(int index);
-		int readRecord(string usfId);
-		void deleteRecord(int index);
-		string genRandomHex();
 		
+		/*
+		Precondition: an integer index of a student record in the dao
+		Postcondition: returns record of the student at the specified index in dao
+		*/
+		Student* readRecord(int index);
+		
+		/*
+		Precondition: A file location has been set to save the data
+		Postcondition: student data is saved to the specified file
+		*/
+		int readRecord(string usfId);
+		
+		/*
+		Precondition: an integer index of the student record in the dao
+		Postcondition: the student at the specified index in dao is deleted
+		*/
+		void deleteRecord(int index);
+		
+		/*
+		Precondition: an input string as keyword to search for in the dao
+		Postcondition: a vector of indexes specifiing students that match the keyword search
+		*/
 		vector<int> search(string keyword);
 };
 
